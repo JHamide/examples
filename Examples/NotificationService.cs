bf6b24844008202d7656d97dfe6b94e70070ae67
Jalilpour <Jalilpour@DESKTOP-APDO5GT>
2024-11-26 11:19:58 +0330

    WorkItem#Solve Example

--- Examples/NotificationService.cs
+++ Examples/NotificationService.cs
@@ -1,15 +1,18 @@
 public class NotificationService()
 {
-    public ... Get(string type)
+    public void Get(string type, Action<string> sendNotification)
     {
-        if (type == "Email")
-            return new ...();
-        else if (type == "SMS")
-            return new ...();
-        //else if (type == "Push")
-        //	return new ...();
+        //if (type == "Email")
+        //    return new ...();
+        //else if (type == "SMS")
+        //    return new ...();
+        ////else if (type == "Push")
+        ////	return new ...();
 
-        else
-            throw new ArgumentException("Invalid notification type.");
+        //else
+        //    throw new ArgumentException("Invalid notification type.");
+        sendNotification(type);
     }
 }
+
+public delegate bool Func(string type);
--- Examples/Ticketing.cs
+++ Examples/Ticketing.cs
@@ -1,14 +1,15 @@
 public class TicketService
 {
-    public ... Get(string ticketType)
+    public void Get(string ticketType, Action<string> getTicket)
     {
-        if (ticketType == "Movie")
-            return new ...();
-        else if (ticketType == "Concert")
-            return new ...();
-        else if (ticketType == "Flight")
-            return new ...();
-        else
-            throw new ArgumentException("Invalid ticket type.");
+        //if (ticketType == "Movie")
+        //    return new ...();
+        //else if (ticketType == "Concert")
+        //    return new ...();
+        //else if (ticketType == "Flight")
+        //    return new ...();
+        //else
+        //    throw new ArgumentException("Invalid ticket type.");
+        getTicket(ticketType);
     }
 }
